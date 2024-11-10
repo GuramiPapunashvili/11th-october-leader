88ffc8836d4d7314bb750fad5a7c57c672277e61
GuramiPapunashvili <157598807+GuramiPapunashvili@users.noreply.github.com>
2024-11-10 19:22:13 +0400

    Update script.js

--- November 10th, leader exam/script.js
+++ November 10th, leader exam/script.js
@@ -38,8 +38,8 @@ async function getEmoji(city){
         main.appendChild(img);
         img.style.height = '100px'
         img.style.width = '100px'
-    } else if (cityInfo.weather[0].main === 'Drizzle'){
-            const img = document.createElement('img');
+    } else if(cityInfo.weather[0].main === 'Drizzle'){
+        const img = document.createElement('img');
         img.src = "emojis/raining.png";
         main.appendChild(img);
         img.style.height = '100px'
